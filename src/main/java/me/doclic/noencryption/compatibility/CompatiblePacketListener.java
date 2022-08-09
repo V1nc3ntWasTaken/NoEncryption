6d5a5ac9554271b3214486bc360f05e4b9dc646f
Vincent <79623093+V1nc3ntWasTaken@users.noreply.github.com>
2022-08-08 20:14:25 -0500

    Added a plugin readiness system

--- src/main/java/me/doclic/noencryption/compatibility/CompatiblePacketListener.java
+++ src/main/java/me/doclic/noencryption/compatibility/CompatiblePacketListener.java
@@ -18,65 +18,68 @@ public class CompatiblePacketListener {
     public Object readPacket(ChannelHandlerContext channelHandlerContext, Object packet) { return packet; }
 
     public Object writePacket(ChannelHandlerContext channelHandlerContext, Object packet, ChannelPromise promise) {
-        if (packet instanceof final ClientboundPlayerChatPacket clientboundPlayerChatPacket) {
-            // Code partially by atenfyr https://github.com/atenfyr
-            final Optional<Component> unsignedContent = clientboundPlayerChatPacket.message().unsignedContent();
-            final ChatMessageContent signedContent = clientboundPlayerChatPacket.message().signedContent();
-            final SignedMessageBody signedBody = clientboundPlayerChatPacket.message().signedBody();
-            final ChatType.BoundNetwork chatType = clientboundPlayerChatPacket.chatType();
+        if (NoEncryption.isReady()) {
 
-            // recreate a new packet
-            return new ClientboundPlayerChatPacket(
-                    new PlayerChatMessage(
-                            new SignedMessageHeader(
-                                    new MessageSignature(new byte[0]),
-                                    new UUID(0,0)),
-                            new MessageSignature(new byte[0]),
-                            new SignedMessageBody(
-                                    new ChatMessageContent(
-                                            signedContent.plain(),
-                                            signedContent.decorated()),
-                                    signedBody.timeStamp(),
-                                    0,
-                                    signedBody.lastSeen()),
-                            unsignedContent,
-                            new FilterMask(0)
-                    ),
-                    chatType);
-        }
+            if (packet instanceof final ClientboundPlayerChatPacket clientboundPlayerChatPacket) {
+                // Code partially by atenfyr https://github.com/atenfyr
+                final Optional<Component> unsignedContent = clientboundPlayerChatPacket.message().unsignedContent();
+                final ChatMessageContent signedContent = clientboundPlayerChatPacket.message().signedContent();
+                final SignedMessageBody signedBody = clientboundPlayerChatPacket.message().signedBody();
+                final ChatType.BoundNetwork chatType = clientboundPlayerChatPacket.chatType();
 
-        if (packet instanceof final ClientboundSystemChatPacket clientboundSystemChatPacket) {
-            if (clientboundSystemChatPacket.content() == null) {
-                return clientboundSystemChatPacket;
-            } else {
-                return new ClientboundSystemChatPacket(
-                        ComponentSerializer.parse(clientboundSystemChatPacket.content()),
-                        clientboundSystemChatPacket.overlay());
-            }
-        }
-
-        if (packet instanceof final ClientboundPlayerChatHeaderPacket clientboundPlayerChatHeaderPacket) {
-            return new ClientboundPlayerChatHeaderPacket(
-                    new SignedMessageHeader(
-                            new MessageSignature(new byte[0]),
-                            new UUID(0,0)),
-                    new MessageSignature(new byte[0]),
-                    clientboundPlayerChatHeaderPacket.bodyDigest()
-            );
-        }
-
-        if (packet instanceof final ClientboundServerDataPacket clientboundServerDataPacket) {
-            if (NoEncryption.getPlugin().getConfig().getBoolean("disable-unsecure-banner", true)) {
                 // recreate a new packet
-                return new ClientboundServerDataPacket(
-                        clientboundServerDataPacket.getMotd().orElse(null),
-                        clientboundServerDataPacket.getIconBase64().orElse(null),
-                        clientboundServerDataPacket.previewsChat(),
-                        true
+                return new ClientboundPlayerChatPacket(
+                        new PlayerChatMessage(
+                                new SignedMessageHeader(
+                                        new MessageSignature(new byte[0]),
+                                        new UUID(0, 0)),
+                                new MessageSignature(new byte[0]),
+                                new SignedMessageBody(
+                                        new ChatMessageContent(
+                                                signedContent.plain(),
+                                                signedContent.decorated()),
+                                        signedBody.timeStamp(),
+                                        0,
+                                        signedBody.lastSeen()),
+                                unsignedContent,
+                                new FilterMask(0)
+                        ),
+                        chatType);
+            }
+
+            if (packet instanceof final ClientboundSystemChatPacket clientboundSystemChatPacket) {
+                if (clientboundSystemChatPacket.content() == null) {
+                    return clientboundSystemChatPacket;
+                } else {
+                    return new ClientboundSystemChatPacket(
+                            ComponentSerializer.parse(clientboundSystemChatPacket.content()),
+                            clientboundSystemChatPacket.overlay());
+                }
+            }
+
+            if (packet instanceof final ClientboundPlayerChatHeaderPacket clientboundPlayerChatHeaderPacket) {
+                return new ClientboundPlayerChatHeaderPacket(
+                        new SignedMessageHeader(
+                                new MessageSignature(new byte[0]),
+                                new UUID(0, 0)),
+                        new MessageSignature(new byte[0]),
+                        clientboundPlayerChatHeaderPacket.bodyDigest()
                 );
             }
-        }
 
+            if (packet instanceof final ClientboundServerDataPacket clientboundServerDataPacket) {
+                if (NoEncryption.getPlugin().getConfig().getBoolean("disable-unsecure-banner", true)) {
+                    // recreate a new packet
+                    return new ClientboundServerDataPacket(
+                            clientboundServerDataPacket.getMotd().orElse(null),
+                            clientboundServerDataPacket.getIconBase64().orElse(null),
+                            clientboundServerDataPacket.previewsChat(),
+                            true
+                    );
+                }
+            }
+
+        }
 
         return packet;
 
